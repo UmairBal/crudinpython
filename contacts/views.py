3eb8ffba83fa10021dc08bbe7c06583bc38d0d65
UmairBal <ubaloch1313@gmail.com>
2024-11-21 04:04:41 +0530

    first commit

---
+++ contacts/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Contact
+from .serializers import ContactSerializer
+from .models import Contact
+from .serializers import ContactSerializer
+
+
+class ContactViewSet(ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+# .order_by('-id')
